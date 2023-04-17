615b6a15442543f84ea35c8f436e0c107a1652b0
cramfull <130737411+cramfull@users.noreply.github.com>
2023-04-17 23:12:14 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,31 @@
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+
+	
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int sum=1;
+		int k=0;
+		while(sum<N) {
+		 sum= sum + 6*k;
+		k++;
+		}
+		if(k==0){
+      System.out.println("1");
+    }else{
+		System.out.println(k);
+    }
+		
+		
+
+
+	}	
+
+}
