fae2d598846baf0314c4492b72c744b4cc9376c9
cramfull <130737411+cramfull@users.noreply.github.com>
2023-04-21 16:16:46 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -2,9 +2,8 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.ArrayList;
 import java.util.Arrays;
+
 
 
 
@@ -13,16 +12,37 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String []N = (br.readLine()).split("");
-		ArrayList als = new ArrayList(Arrays.asList(N));
-		int max;
+		String [] N = br.readLine().split("");
 		
-		als.sort(Comparator.reverseOrder());
 		
+		String tmp="";
+		for(int i=0; i<N.length-1; i++) {
+			for(int j=i+1; j<N.length; j++) {
+				if(Integer.parseInt(N[i])<Integer.parseInt(N[j])) {
+					tmp = N[i];
+					N[i]=N[j];
+					N[j]=tmp;
+				}
+			}
+		}
 		String result="";
-		for(int i=0; i<als.size(); i++) {
-			result+=als.get(i);
+		for(int k=0; k<N.length; k++) {
+			result+=N[k];
 		}
 		System.out.println(result);
-    }
+		
+		
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
