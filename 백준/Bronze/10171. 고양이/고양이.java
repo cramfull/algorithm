ecc4bb18650e05d835b260fd6e2df12167050fe4
cramfull <130737411+cramfull@users.noreply.github.com>
2023-04-24 12:41:17 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,38 @@
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+
+
+
+
+	
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
