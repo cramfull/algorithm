401d9c2c9bad5c42d9e221b3acc5f5f8222ac932
cramfull <130737411+cramfull@users.noreply.github.com>
2023-05-19 00:51:45 +0900

    [Bronze V] Title: 이상한 기호, Time: 216 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Scanner scn = new Scanner(System.in);
+	int A = scn.nextInt();
+	int B = scn.nextInt();
+	int result = (A+B)*(A-B);
+	System.out.println(result);
+		
+	}
+
+}
