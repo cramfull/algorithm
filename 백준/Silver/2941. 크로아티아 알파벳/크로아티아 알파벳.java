9e39c14845be55e8c782830202ce820054929599
cramfull <130737411+cramfull@users.noreply.github.com>
2023-05-25 15:45:05 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 120 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,54 +1,44 @@
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		String str = br.readLine();
-		int result = str.length();
-		for(int i=0; i<str.length(); i++) {
-			if(i>0) {
-				switch(str.charAt(i)) {
-				case '=':
-					if(str.charAt(i-1)=='c') {
-						result--;
-					} else if(str.charAt(i-1)=='s') {
-						result--;
-					} else if(str.charAt(i-1)=='z') {
-						if(i>1) {
-							if(str.charAt(i-2)=='d') {
-								result--;
-							}
-						}
-						result--;
-					}
-					break;
-				case '-':
-					if(str.charAt(i-1)=='c') {
-						result--;
-					}else if(str.charAt(i-1)=='d') {
-						result--;
-					}
-					break;
-				case 'j':
-					if(str.charAt(i-1)=='l') {
-						result--;
-					}else if(str.charAt(i-1)=='n') {
-						result--;
-					}
-					break;
-				}
-			}
-		}
-		System.out.println(result);
-		
-		
-	}
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int count = 0;
+        boolean check = false;
+        String[] strArr = new String[]{"c=","c-","dz=","d-","lj","nj","s=","z="};
+
+        for(int i = 0; i<s.length(); i++){
+            for(int j = 0; j<strArr.length; j++){
+                if(s.charAt(i) == 'd' && i <= s.length()-3) {
+                    if (s.charAt(i + 1) == strArr[2].charAt(1) && s.charAt(i + 2) == strArr[2].charAt(2)) {
+                        i += 2;
+                        count++;
+                        check = true;
+                        break;
+                    }
+                }
+                if(i != s.length()-1) {
+                    if (s.charAt(i) == strArr[j].charAt(0) &&
+                            s.charAt(i + 1) == strArr[j].charAt(1) && j != 2) {
+                        i += 1;
+                        count++;
+                        check = true;
+                        break;
+                    }
+                }else{
+                    break;
+                }
+            }
+            if(!check){
+                count++;
+            }else{
+                check = false;
+            }
+        }
+        System.out.print(count);
+    }
 }
