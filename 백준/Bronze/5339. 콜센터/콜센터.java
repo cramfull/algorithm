eeb745fa19cda565f6f463230779c19567213f87
cramfull <130737411+cramfull@users.noreply.github.com>
2023-06-09 23:20:55 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		
+		
+		
+		
+	}
+}
