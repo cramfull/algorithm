a1493591d50298c0c06414bfbfde43a308ea6429
cramfull <130737411+cramfull@users.noreply.github.com>
2023-06-14 14:31:55 +0900

    [Silver V] Title: 다각형의 대각선, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/3049. 다각형의 대각선/다각형의 대각선.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int result = 0;
+		
+		result = N*(N-1)*(N-2)*(N-3)/24;
+		
+		System.out.println(result);
+		
+		
+		
+		
+	}
+}
