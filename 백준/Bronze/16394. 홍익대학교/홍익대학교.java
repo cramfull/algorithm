542cd3f30d94df1e2d01c883d16c6f0b823671aa
cramfull <130737411+cramfull@users.noreply.github.com>
2023-06-23 23:33:34 +0900

    [Bronze V] Title: 홍익대학교, Time: 212 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int N = scn.nextInt();
+		N-=1946;
+		
+		System.out.println(N);
+	}
+	
+	
+	
+}
