2b1135955c312d6d64e5a3c09f20aa57eda7385d
cramfull <130737411+cramfull@users.noreply.github.com>
2023-09-02 19:15:16 +0900

    [Silver II] Title: 최소 힙, Time: 352 ms, Memory: 26476 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,36 @@
+
+
+// 제출 X 
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		int N = Integer.parseInt(br.readLine());
+		int k=0;
+		for(int i=0; i<N; i++) {
+			k = Integer.parseInt(br.readLine());
+			
+			if(k==0) {
+				if(pq.size()==0) {
+					bw.write(String.valueOf(0));
+				}else {
+					bw.write(String.valueOf(pq.poll()));
+				}
+				bw.newLine();
+
+			}else {
+				pq.add(k);
+			}
+			
+		}
+		bw.flush();
+		bw.close();
+	}
+}
