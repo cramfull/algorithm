58093dcf0046ea932fe0eb4700c96fc47b7b92cf
cramfull <130737411+cramfull@users.noreply.github.com>
2023-09-12 19:37:55 +0900

    [Silver III] Title: 문자열 집합, Time: 416 ms, Memory: 31272 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,31 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		Set<String> set = new HashSet<>();
+		
+		for(int i=0; i<N; i++) {
+			set.add(br.readLine());
+		}
+		int result = 0;
+		for(int i=0; i<M; i++) {
+			if(set.contains(br.readLine())) {
+				result++;
+			}
+		}
+		System.out.println(result);
+		
+		
+	}
+	
+}
