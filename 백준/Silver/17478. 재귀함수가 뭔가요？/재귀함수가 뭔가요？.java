121dc744e3728c6f9609d52b3cd6eec154e51c39
cramfull <130737411+cramfull@users.noreply.github.com>
2023-10-13 15:44:32 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 140 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -21,33 +21,28 @@ public class Main {
     	}
 		sb.append("\"재귀함수가 뭔가요?\""+"\n");
     	if(n == N) {
-    		for(int i=0;i<n;i++) {
-        		sb.append("____");
-        	}
+    		
+    		under(n);
     		sb.append("\"재귀함수는 자기 자신을 호출하는 함수라네\""+"\n");
-    		for(int i=0;i<n;i++) {
-        		sb.append("____");
-        	}
+    		under(n);
     		sb.append("라고 답변하였지."+"\n");
     	}else {
-    		for(int i=0;i<n;i++) {
-        		sb.append("____");
-        	}
+    		under(n);
     		sb.append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
-    		for(int i=0;i<n;i++) {
-        		sb.append("____");
-        	}
-    		
+    		under(n);
     		sb.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
-    		for(int i=0;i<n;i++) {
-        		sb.append("____");
-        	}
+    		under(n);
     		sb.append( "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
     		whatIs(n+1);
-    		for(int i=0;i<n;i++) {
-        		sb.append("____");
-        	}
+    		under(n);
     		sb.append("라고 답변하였지."+"\n");
     	}
     }
+    static void under(int n) {
+    	for(int i=0;i<n;i++) {
+    		sb.append("____");
+    	}
+    }
+    
+    
 }
