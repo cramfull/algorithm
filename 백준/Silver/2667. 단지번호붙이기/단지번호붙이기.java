ee531c6cbba4e47fefb1b3ed0113e266fc1ea640
cramfull <130737411+cramfull@users.noreply.github.com>
2023-10-31 16:20:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 144 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,66 +4,56 @@ import java.math.*;
 import java.util.*;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
 	static int N;
-	static int count;
 	static int graph [][];
-	static boolean visited [][];
-	static int dx [] = new int[] {-1,1,0,0};
-	static int dy [] = new int[] {0,0,-1,1};	// 상 하 좌 우 
+	static boolean visited[][];
+	static int count;
+	static int result;
+	static int [] dx = new int[] { -1,1,0,0 };
+	static int [] dy = new int[] { 0,0,-1,1 };		// 상 하 좌 우
 	
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	N = Integer.parseInt(br.readLine());
     	graph = new int[N][N];
-    	
+    	visited = new boolean[N][N];
     	for(int i=0;i<N;i++) {
     		String [] tmp = br.readLine().split("");
     		for(int j=0;j<N;j++) {
-    			graph[i][j] = Integer.parseInt(tmp[j]);
+    			graph[i][j]=Integer.parseInt(tmp[j]);
     		}
     	}
-    	visited = new boolean[N][N];
+    	result =0;
     	ArrayList<Integer> arr = new ArrayList<>();
-    	int result = 0;
     	for(int i=0;i<N;i++) {
     		for(int j=0;j<N;j++) {
-    			if(graph[i][j]==1&& !visited[i][j]) {
-    				bfs(i,j);
+    	    	count =0;
+    			if(graph[i][j]==1 && !visited[i][j]) {
+    				dfs(i,j);
     				arr.add(count);
     				result++;
     			}
     		}
     	}
     	Collections.sort(arr);
-    	
     	System.out.println(result);
-    	for(int i=0;i<arr.size();i++) {
-    		System.out.println(arr.get(i));
+    	for(int a : arr) {
+    		System.out.println(a);
     	}
-    	
-    	
-    	
-    	
+
     	
     }
-    
-    static void bfs(int x, int y) {
-    	Queue<Point> que = new LinkedList<>();
-    	que.offer(new Point(x,y));
-    	graph[x][y]=0;
-		count = 0;
-
-    	while(!que.isEmpty()) {
-    		count++;
-    		Point now = que.poll();
-    		for(int i=0;i<4;i++) {
-    			int nx = now.x + dx[i];
-    			int ny = now.y + dy[i];
-    			if(nx>=0 && nx<N && ny>=0 && ny<N && graph[nx][ny]==1&&!visited[nx][ny]) {
-    				graph[nx][ny]=0;
-    				que.offer(new Point(nx,ny));
-    			}
+    static void dfs(int x, int y) {
+    	Point now = new Point(x,y);
+    	visited[x][y]=true;
+    	count++;
+    	for(int i=0;i<4;i++) {
+    		int nx = now.x + dx[i];
+    		int ny = now.y + dy[i];
+    		if(nx>=0 && nx<N && ny>=0 && ny<N && graph[nx][ny]==1 && !visited[nx][ny]) {
+    			visited[nx][ny]=true;
+    			dfs(nx,ny);
+    			
     		}
     	}
     	
@@ -72,13 +62,15 @@ public class Main {
     
     
     static class Point{
-    	int x, y;
-    	
+    	int x,y;
     	public Point(int x, int y) {
-    		this.x = x;
-    		this.y = y;
+    		this.x=x;
+    		this.y=y;
     	}
-    	
     }
     
+    
+    
 }
+
+
