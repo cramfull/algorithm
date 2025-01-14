9da4ecd876d78ad8c33f288e66bc6a784929568e
cramfull <130737411+cramfull@users.noreply.github.com>
2025-01-14 16:13:57 +0900

    [Silver V] Title: 부분 문자열, Time: 160 ms, Memory: 18320 KB -BaekjoonHub

--- 백준/Silver/6550. 부분 문자열/부분 문자열.java
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.java
@@ -1,73 +1,46 @@
 
-import java.io.*;
-import java.math.*;
-import java.util.*;
-import java.util.Map.Entry;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	
     public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	
-    	
-    	String input = "";
-    	
-    	while((input = br.readLine())!= null) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 입력 받기
+        String readLine;
+        while ((readLine = br.readLine()) !=null) {
 
-        	StringTokenizer str = new StringTokenizer(input);
-        	
-        	String s = str.nextToken();
-        	String t = str.nextToken();
-    		System.out.println(sub(s,t));
-        	
-        	
-        	
-        	
-    		
-    	}
-    	
-    	
-    	
-    	
-    	
-    	
-    	
-    	
+            String [] arr = readLine.split(" ");
+            String s = arr[0];
+//            if (s.isEmpty()) {
+//                System.out.println("Yes");
+//                continue;
+//            }
+            String t = arr[1];
+
+
+
+            int idx = 0;
+            // t 문자열 length 만큼 반복
+            for (int i = 0; i < t.length(); i++) {
+                char sChar = s.charAt(idx);
+                char tChar = t.charAt(i);
+            // t 문자열 s의 idx 문자열 일치 시 ++
+                if (sChar==tChar){
+                   idx++;
+                    if (idx == s.length()) {
+                        break;
+                    }
+                }
+            }
+            // idx == s.length + 1 인 경우
+            if (idx == s.length()) {
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
     }
-    static String sub(String s, String t) {
-    	boolean [] arr = new boolean[s.length()];
-		int at = 0;
-    	for(int i=0;i<s.length();) {
-    		for(int j=at;j<t.length();j++) {
-        		char sChar=s.charAt(i);
-    			char tChar = t.charAt(j);
-    			if(sChar==tChar) {
-    				arr[i]=true;
-        			at = j;
-        			i++;
-        			if(i==s.length()) {
-        				break;
-        			}
-    			}
-    			
-    		}
-    		i++;
-    	}
-    	for(int i=0;i<arr.length;i++) {
-    		if(!arr[i]) {
-    			
-    			return "No";
-    		}
-    	}
-    	return "Yes";
-    	
-    	
-    }
-    
-    
-    
-    
-    
 }
-
