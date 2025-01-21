f60027f3f21f7c0f1dc31a45991955371b8d1e02
cramfull <130737411+cramfull@users.noreply.github.com>
2025-01-21 16:52:46 +0900

    [Silver II] Title: 스택 수열, Time: 308 ms, Memory: 26828 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -7,56 +7,33 @@ import java.util.Stack;
 public class Main {
     // 1/ 2/ 3 4 5/ 4<- NO
     // 4 3 6 8 7 5 2 1
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-
+        
         Stack<Integer> inStack = new Stack<>();
-        Stack<Integer> outStack = new Stack<>();
-
-        for (int i = N; i>0; i--) {
-            outStack.push(i);
-        }
-
-        StringBuilder sb = new StringBuilder();
+        int current = 1;
         int idx = 0;
 
-        while (idx < arr.length) {
+        StringBuilder sb = new StringBuilder();
+        while (idx < N) {
             int target = arr[idx];
-            if (!outStack.isEmpty()) {
-                while (target > outStack.peek()) {
-                    inStack.push(outStack.pop());
-                    sb.append("+").append("\n");
-                }
-            }
-            if(!outStack.isEmpty()){
-                if (outStack.peek() == target) {
-                    outStack.pop();
-                    sb.append("+").append("\n").append("-").append("\n");
-                    idx++;
-                    continue;
-                }
-            }
-            if (!inStack.isEmpty()) {
-                if (inStack.peek() == target) {
-                    inStack.pop();
-                    sb.append("-").append("\n");
-                    idx++;
-                } else {
-                    sb = new StringBuilder("NO");
-                    System.out.println(sb);
-                    return;
-                }
+            if (target >= current) {
+                inStack.push(current++);
+                sb.append("+").append("\n");
+            } else if (target == inStack.peek()) {
+                inStack.pop();
+                sb.append("-").append("\n");
+                idx++;
+            } else {
+                sb = new StringBuilder("NO");
+                break;
             }
         }
         System.out.println(sb);
     }
-
 }
