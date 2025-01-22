bdb98a7c0f12fa24b9734428ce669051240d73b4
cramfull <130737411+cramfull@users.noreply.github.com>
2025-01-22 17:58:58 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,59 +1,57 @@
-
 import java.io.*;
 import java.math.*;
 import java.util.*;
 import java.util.Map.Entry;
 
+
+// dfs로 다시풀어보기 
+
 public class Main {
 	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-	static int [] result;
-	static boolean virus[];
 	static int count;
+	static boolean [] virus;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int vertex = Integer.parseInt(br.readLine());
 		int edge = Integer.parseInt(br.readLine());
 		
-		for(int i=0;i<vertex+1; i++) {
+		StringTokenizer str ;
+		for(int i=0;i<vertex+1;i++) {
 			graph.add(new ArrayList<>());
 		}
-		
 		for(int i=0;i<edge;i++) {
-			StringTokenizer str = new StringTokenizer(br.readLine());
+			str = new StringTokenizer(br.readLine());
 			int st = Integer.parseInt(str.nextToken());
 			int ed = Integer.parseInt(str.nextToken());
 			graph.get(st).add(ed);
 			graph.get(ed).add(st);
 		}
-		virus = new boolean[vertex+1];
 		count = 0;
-		
-		bfs();
+		virus = new boolean[vertex+1];
+		dfs(1);
 		for(int i=2;i<virus.length;i++) {
 			if(virus[i]) {
 				count++;
 			}
 		}
 		System.out.println(count);
-	}
-	static void bfs() {
-		Queue<Integer> que = new LinkedList<>();
-		virus[1]=true;
-		que.offer(1);
 		
-		while(!que.isEmpty()) {
-			int k = que.poll();
-			for(int i=0;i<graph.get(k).size();i++) {
-				int nextVertex = graph.get(k).get(i);
-				if(!virus[nextVertex]) {
-					que.offer(nextVertex);
-					virus[nextVertex] = true;
-				}
+	}
+	
+	static void dfs(int start) {
+		virus[start] = true;
+		for(int i=0;i<graph.get(start).size();i++) {
+			int newVertex = graph.get(start).get(i);
+			if(!virus[newVertex]) {
+				dfs(newVertex);
 			}
 		}
-		
 	}
+	
+	
+	
+
 	
 	
 }
