cbd67320684ee990d57bbaaf238740fed8b6ef10
cramfull <130737411+cramfull@users.noreply.github.com>
2025-01-23 22:22:04 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,21 +1,74 @@
 
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		
-		if(N%2==0) {
-			System.out.println("CY");
-		}else {
-			System.out.println("SK");
-		}
-	
-	}
+
+    // ㅅ    ㅊ    ㅅ   ㅊ   ㅅ
+
+    // #1   SK
+    // 1
+    // dp[1] = 1
+
+    // #2   CY
+    // 1   1
+    // dp[2] = 1
+
+    // #3   SK
+    //  1   1     1
+    //  3
+    // dp[3] = 2
+
+    //  #4  CY
+    //  1     1     1     1
+    //  1    3
+    //  3    1
+    // dp[4] = 3
+
+    // #5   SK
+    // 1    1     1    1   1
+    // 1    1     3
+    // 1    3     1
+    // 3    1     1
+    // dp[5] = 4
+
+    // #6 CY
+    // 1   1    1    1   1   1
+    // 1   1    1    3
+    // 1   1    3    1
+    // 1   3    1    1
+    // 3   1    1    1
+    // 3   3
+    // dp[6]= 6
+
+    // #7 SK
+    // 1  1   1   1  1  1   1
+    // 1  1   1   1  3
+    // 1  1   1   3  1
+    // 1  1   3   1  1
+    // 1  3   1   1  1
+    // 3  1   1   1  1
+    // 1  3   3
+    // 3  1   3
+    // 3  3   1
+
+    // 1 2 3 4 5 6 7
+    // 1 1 2 3 4 6 9
+
+    // n >= 4
+    // dp[n] = dp[n-2] + dp[n-3] +dp[n-4]
+
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int [] dp = new int[N+4];
+        if(N%2==0){
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+
+    }
 }
