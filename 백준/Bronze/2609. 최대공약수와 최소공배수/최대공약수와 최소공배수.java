80c9e289bcadd9dd208ab23c96ae408e56b859c6
cramfull <130737411+cramfull@users.noreply.github.com>
2025-01-30 18:03:46 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,45 +1,27 @@
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer str = new StringTokenizer(br.readLine());
-		int input1 = Integer.parseInt(str.nextToken());
-		int input2 = Integer.parseInt(str.nextToken());
-		
-		int maxV = 0;
-		int minV = 0;
-		if(input1<input2) {
-			maxV = input2;
-			minV = input1;
-		}else{
-			maxV = input1;
-			minV = input2;
-		}
-		
-		int gongY = gcd(maxV,minV);
-		int gongB = gongY*(maxV/gongY)*(minV/gongY);
-		
-		System.out.println(gongY);
-		System.out.println(gongB);
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(str.nextToken());
+        int b = Integer.parseInt(str.nextToken());
 
-	}
+        int k = gcd(Math.max(a, b), Math.min(a, b));
+        StringBuilder sb = new StringBuilder();
+        sb.append(k).append("\n").append((a/k)*k*(b/k));
+        System.out.println(sb);
+    }
 
-	static int gcd(int a, int b) {
-		
-		if(b==0) {
-			return a;
-		}
-		
-		int r = a%b;
-		int na = b;
-		int nb = r;
-		
-		return gcd(na,nb);
-	}
-	
+    static int gcd(int a, int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b, a%b);
+    }
 }
