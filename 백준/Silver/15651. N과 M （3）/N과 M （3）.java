4da05eff76347563d9cca13bcd7d07dc99c32f35
cramfull <130737411+cramfull@users.noreply.github.com>
2025-07-20 11:44:07 +0900

    [Silver III] Title: N과 M (3), Time: 424 ms, Memory: 68532 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,43 +1,40 @@
 
-import java.io.*;
-import java.math.*;
 import java.util.*;
-import java.util.Map.Entry;
+import java.io.*;
 
 public class Main {
-	
-	
-	static int [] arr;
-	static int N,M;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[M+1];
-		backTracking(0,N,M, sb);
-		
-		System.out.println(sb.toString());
-		
-		
-		
-	}
-	static void backTracking(int x, int N , int M, StringBuilder sb) {
-		if(x==M) {
-			for(int i=0;i<M;i++) {
-				sb.append(arr[i]+" ");
-			}
-			sb.append("\n");
-			return;
-		}else {
-			for(int i=1; i<=N; i++) {
-				arr[x] = i;
-				backTracking(x+1, N, M, sb);
-			}
-		}
-		
-	}
-	
+
+    static int N,M;
+    static int [] arr;
+    static StringBuilder sb;
+
+    public static void main(String [] args) throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(str.nextToken());
+        M = Integer.parseInt(str.nextToken());
+        arr = new int[M];
+        sb = new StringBuilder();
+
+        backTracking(0);
+        System.out.println(sb.toString());
+    }
+
+    static void backTracking(int depth){
+        if (depth == M) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            backTracking(depth + 1);
+        }
+
+    }
+
+
 }
