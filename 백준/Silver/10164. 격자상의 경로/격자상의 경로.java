6875e53d4082923e85ecbdb2724fae151c77bb86
cramfull <130737411+cramfull@users.noreply.github.com>
2025-07-22 02:01:10 +0900

    [Silver II] Title: 격자상의 경로, Time: 104 ms, Memory: 14264 KB, Score: 32 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -0,0 +1,48 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, K;
+    static int [][] arr;
+    static int [] dx = {0, -1};
+    static int [] dy = {-1, 0};
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(str.nextToken());
+        M = Integer.parseInt(str.nextToken());
+        K = Integer.parseInt(str.nextToken());
+        arr = new int[N+1][M+1];
+        int result = 0;
+        if (K == 0) {
+            result = getResult(1, 1, N, M);
+        }else{
+            int toMiddle = getResult(1, 1, K/M+1, K%M);
+            int toEnd = getResult(K/M+1, K%M, N,M);
+
+            result = toEnd;
+        }
+
+
+        System.out.println(result);
+    }
+
+    static int getResult(int startX, int startY, int endX, int endY){
+        for (int i = startX; i <= endX; i++) {
+            for (int j = startY; j <= endY; j++) {
+                for (int k = 0; k < 2; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if(nx>0 && ny>0 && nx<=endX && ny<=endY){
+                        arr[i][j]=arr[i][j-1]+arr[i-1][j];
+                    }else{
+                        arr[i][j]=1;
+                    }
+                }
+            }
+        }
+        return arr[endX][endY];
+    }
+}
