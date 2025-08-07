43c9804a44382e3a1a25c160192b5a3e8c78e83a
cramfull <130737411+cramfull@users.noreply.github.com>
2025-08-07 22:01:19 +0900

    [D3] Title: 햄버거 다이어트, Time: 2,603 ms, Memory: 138,336 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -32,9 +32,9 @@ import java.io.*;
    사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
    이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
  */
-class Solution
-{
-	static ArrayList<Integer> results;
+class Solution {
+	
+	static int result;
 		
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,58 +45,45 @@ class Solution
 			str = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(str.nextToken());
 			int L = Integer.parseInt(str.nextToken());
-			Pair [] ingredients = new Pair[N];
+			int [] ingredients = new int[N];
+			int [] kcals = new int[N];
 			boolean [] visited = new boolean[N];
-			results = new ArrayList<>();
+			result = 0;
 			for(int i=0;i<N;i++) {
 				str = new StringTokenizer(br.readLine());
-				int T = Integer.parseInt(str.nextToken());
-				int K = Integer.parseInt(str.nextToken());
-				ingredients[i] = new Pair(T, K);
+				ingredients[i] = Integer.parseInt(str.nextToken());
+				kcals[i]= Integer.parseInt(str.nextToken());
 			}
 			
-			powerSet(ingredients, visited, N, L, 0);
-			
-			Collections.sort(results);
-			int result = results.get(results.size()-1);
+			powerSet(ingredients, kcals, visited, N, L, 0);
+
 			sb.append(String.format("#%d %d", tc, result)).append("\n");
 			
 		}
 		System.out.println(sb.toString());
 	}
 	
-	static void powerSet(Pair [] ingredients, boolean [] visited, int N, int L, int depth) {
+	static void powerSet(int [] ingredients, int [] kcals, boolean [] visited, int N, int L, int depth) {
 		if(depth == N) {
 			int scoreSum = 0;
 			int kcalSum = 0;
 			for(int i=0;i<N;i++) {
 				if(visited[i]) {
-					Pair ingredient = ingredients[i];
-					scoreSum+= ingredient.score;
-					kcalSum += ingredient.kcal;
-					if(kcalSum>L) {
-						return;
+					scoreSum+= ingredients[i];
+					kcalSum += kcals[i];
+					if(kcalSum<=L) {
+						result = Math.max(result,scoreSum);
 					}
 				}
 			}
-			results.add(scoreSum);
 			return;
 		}
 		
 		visited[depth] = false;
-		powerSet(ingredients, visited, N, L, depth+1);
+		powerSet(ingredients, kcals, visited, N, L, depth+1);
 		
 		visited[depth] = true;
-		powerSet(ingredients, visited, N, L, depth+1);
+		powerSet(ingredients, kcals, visited, N, L, depth+1);
 	}
 	
-	static class Pair{
-		int score;
-		int kcal;
-		
-		public Pair(int score, int kcal) {
-			this.score = score;
-			this.kcal = kcal;
-		}
-	}
 }
