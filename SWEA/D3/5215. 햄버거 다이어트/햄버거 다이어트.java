50891ae92e944a02ef76ade97652d9b08aa461b0
cramfull <130737411+cramfull@users.noreply.github.com>
2025-08-07 23:33:12 +0900

    [D3] Title: 햄버거 다이어트, Time: 703 ms, Memory: 27,640 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -32,8 +32,7 @@ import java.io.*;
    사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
    이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
  */
-class Solution
-{
+class Solution {
 	
 	static int N,L,result;
 	static int [] ingredients, kcals;
@@ -89,5 +88,4 @@ class Solution
 		visited[depth] = true;
 		powerSet(depth+1);
 	}
-	
 }
