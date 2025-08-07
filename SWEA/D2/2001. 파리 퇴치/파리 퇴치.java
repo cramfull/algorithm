04946c39509e5654770ff7c7d20a6b232c1fe400
cramfull <130737411+cramfull@users.noreply.github.com>
2025-08-08 01:30:36 +0900

    [D2] Title: 파리 퇴치, Time: 74 ms, Memory: 24,320 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -2,53 +2,58 @@ import java.util.*;
 import java.io.*;
 
 class Solution{
- 	
-    static int [][] board;
-     
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int TC = Integer.parseInt(br.readLine());
-        StringTokenizer str;
-        for(int tc=1; tc<=TC; tc++) {
-            str = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(str.nextToken());
-            int M = Integer.parseInt(str.nextToken());
-            board = new int[N][N];
-//            for(int i=0;i<N;i++) {
-//                board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-//            }
+
+	static int N,M, maxSum;
+	static int [][] board;
+	static int [][] sumBoard;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int TC = Integer.parseInt(br.readLine());
+		
+		for(int tc=1; tc<=TC; tc++) {
+			StringTokenizer str = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(str.nextToken());
+			M = Integer.parseInt(str.nextToken());
+			maxSum = Integer.MIN_VALUE;
+			board = new int[N][N];
+			sumBoard = new int[N][N+1];
+//			for(int i=0;i<N;i++) {
+//				board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+//			}
             for(int i=0;i<N;i++) {
             	String [] line = br.readLine().split(" ");
             	for(int j=0;j<N;j++) {
             		board[i][j] = Integer.parseInt(line[j]);
             	}
             }
-            
-            sb.append("#").append(tc).append(" ").append(calculateFlies(N, M)).append("\n");
-        }
-        System.out.println(sb.toString());
-    }
-     
-    
-    static int calculateFlies(int N, int M) {
-        int canKillMaximum = Integer.MIN_VALUE;
-        for(int i=0;i<=N-M;i++) {
-            for(int j=0;j<=N-M;j++) {
-                canKillMaximum = Math.max(canKillMaximum, killFlies(i,j,M));
-            }
-        }
-        return canKillMaximum;
-    }
-     
-    
-    static int killFlies(int x, int y, int M) {
-        int sum=0;
-        for(int i=x;i<x+M;i++) {
-            for(int j=y;j<y+M;j++) {
-                sum+=board[i][j];
-            }
-        }
-        return sum;
-    }
+			
+			saveSumBoard();
+			
+			for(int i=0;i<=N-M;i++) {
+				for(int j=M;j<=N;j++) {
+					maxSum = Math.max(maxSum, calculateKillFlies(i,j));
+				}
+			}
+			sb.append(String.format("#%d %d", tc, maxSum)).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
+	static void saveSumBoard() {
+		for(int i=0;i<N;i++) {
+			for(int j=1;j<=N;j++) {
+				sumBoard[i][j] = sumBoard[i][j-1]+board[i][j-1];
+			}
+		}
+	}
+	
+	static int calculateKillFlies(int x, int y) {
+		int sum = 0;
+		for(int i=x;i<x+M;i++) {
+			sum += sumBoard[i][y]-sumBoard[i][y-M];
+		}
+		return sum;
+	}
 }
