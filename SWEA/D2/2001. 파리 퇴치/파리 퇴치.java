a5e9611cb7c9ae7775208c92bc2844e220169884
cramfull <130737411+cramfull@users.noreply.github.com>
2025-08-08 00:55:17 +0900

    [D2] Title: 파리 퇴치, Time: 144 ms, Memory: 32,640 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,42 +1,7 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
 import java.util.*;
 import java.io.*;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
 class Solution{
-
-	static int N,M, maxSum;
-	static int [][] board;
-	static int [][] sumBoard;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,16 +10,15 @@ class Solution{
 		
 		for(int tc=1; tc<=TC; tc++) {
 			StringTokenizer str = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(str.nextToken());
-			M = Integer.parseInt(str.nextToken());
-			maxSum = Integer.MIN_VALUE;
-			board = new int[N][N];
-			sumBoard = new int[N][N+1];
+			int N = Integer.parseInt(str.nextToken());
+			int M = Integer.parseInt(str.nextToken());
+			int maxSum = Integer.MIN_VALUE;
+			int [][] board = new int[N][N];
+			int [][]sumBoard = new int[N][N+1];
 			for(int i=0;i<N;i++) {
 				board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 			}
 			
-//			saveSumBoard();
 			for(int i=0;i<N;i++) {
 				for(int j=1;j<=N;j++) {
 					sumBoard[i][j] = sumBoard[i][j-1]+board[i][j-1];
@@ -67,28 +31,12 @@ class Solution{
 					for(int k=i;k<i+M;k++) {
 						sum+= sumBoard[k][j]-sumBoard[k][j-M];
 					}
-//					maxSum = Math.max(maxSum, calculateKillFlies(i,j));
 					maxSum = Math.max(maxSum, sum);
 				}
 			}
 			sb.append(String.format("#%d %d", tc, maxSum)).append("\n");
 		}
 		System.out.println(sb.toString());
+	
 	}
-	
-//	static void saveSumBoard() {
-//		for(int i=0;i<N;i++) {
-//			for(int j=1;j<=N;j++) {
-//				sumBoard[i][j] = sumBoard[i][j-1]+board[i][j-1];
-//			}
-//		}
-//	}
-	
-//	static int calculateKillFlies(int x, int y) {
-//		int sum = 0;
-//		for(int i=x;i<x+M;i++) {
-//			sum += sumBoard[i][y]-sumBoard[i][y-M];
-//		}
-//		return sum;
-//	}
 }
