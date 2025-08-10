aa176afe1212b4fc1bb11f00ddc0ba6414175d53
cramfull <130737411+cramfull@users.noreply.github.com>
2025-08-11 02:34:33 +0900

    [Silver II] Title: 결혼식, Time: 172 ms, Memory: 19068 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -1,66 +1,63 @@
 import java.io.*;
 import java.util.*;
 
-/*
- * 그래프 순회
- * count = count+1, dist에 저장
- * dist[i]<=2 인 경우 
- */
 public class Main {
-
-	static int N, M, totalCount;
-	static int [] dist;
-	static boolean [] visited;
+	static int N,M, totalCount;
 	static ArrayList<ArrayList<Integer>> graph;
-	
+	static boolean [] visited;
+	static int [] dist;
+
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer str;
 		
+		graph = new ArrayList<>();
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
-		totalCount = 0;
-		graph = new ArrayList<ArrayList<Integer>>();
-		dist = new int[N+1];
 		visited = new boolean[N+1];
+		dist = new int[N+1];
+		
 		for(int i=0;i<N+1;i++) {
 			graph.add(new ArrayList<>());
 		}
-
+		
 		for(int i=0;i<M;i++) {
-			str = new StringTokenizer(br.readLine());
+			StringTokenizer str = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(str.nextToken());
 			int end = Integer.parseInt(str.nextToken());
+			
 			graph.get(start).add(end);
 			graph.get(end).add(start);
 		}
 		
-		bfs();
-		
+		bfs(1);
+//		int cnt = 0;
+//		for(int i=1;i<dist.length-1;i++) {
+//			if(dist[i]>0 && dist[i]<=2) {
+//				cnt++;
+//			}
+//		}
 		System.out.println(totalCount);
-	
+		
 	}
-	
-	static void bfs() {
+	static void bfs(int start) {
 		Queue<Integer> que = new LinkedList<>();
-		dist[1] = 0;
-		visited[1] = true;
-		que.offer(1);
+		visited[start] = true;
+		dist[start] = 0;
+		que.offer(start);
+		
 		while(!que.isEmpty()) {
 			int now = que.poll();
 			for(int i=0;i<graph.get(now).size();i++) {
 				int nxt = graph.get(now).get(i);
 				if(visited[nxt]) continue;
-				
 				visited[nxt] = true;
 				dist[nxt] = dist[now]+1;
 				if(dist[nxt]<=2) {
 					totalCount++;
 					que.offer(nxt);
-					continue;
 				}
-				return;
 			}
 		}
 	}
+
 }
