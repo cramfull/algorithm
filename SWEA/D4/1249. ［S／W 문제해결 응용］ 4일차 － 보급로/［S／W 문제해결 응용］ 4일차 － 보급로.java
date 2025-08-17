71e4e5201534942de431423681b4bf9321aa0960
cramfull <130737411+cramfull@users.noreply.github.com>
2025-08-17 20:35:52 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 110 ms, Memory: 28,860 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -3,75 +3,70 @@ import java.util.*;
 import java.io.*;
 
 class Solution{
-	static int N;
+	static int N, result;
 	static int [][] map;
 	static boolean [][] visited;
-	static int [][] d = new int[][] {{0,1},{0,-1},{1,0},{-1,0}};
-	
-	public static void main(String args[]) throws Exception{
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 
-		 int TC = Integer.parseInt(br.readLine());
-		 
-		 for(int tc=1;tc<=TC;tc++) {
-			 N = Integer.parseInt(br.readLine());
-			 map = new int[N][N];
-			 visited = new boolean[N][N];
-			 
-			 for(int i=0;i<N;i++) {
-				 String line = br.readLine();
-				 for(int j=0;j<N;j++) {
-					 map[i][j] = line.charAt(j) - '0';
-				 }
-			 }
-			 StringBuilder sb = new StringBuilder();
-			 int result = bfs(new Point(0,0,0));
-			 sb.append("#").append(tc).append(" ").append(result);
-			 
-			 System.out.println(sb.toString());
-		 }
+	static int [] dx = {1,0,-1,0};
+	static int [] dy = {0,1,0,-1};
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int TC = Integer.parseInt(br.readLine());
+		
+		for(int tc=1;tc<=TC;tc++) {
+			N = Integer.parseInt(br.readLine());
+			map = new int[N][N];
+			visited = new boolean[N][N];
+			
+			for(int i=0;i<N;i++) {
+				String line = br.readLine();
+				for(int j=0;j<N;j++) {
+					map[i][j] = line.charAt(j)-'0';
+				}
+			}
+			bfs(new Point(0,0,0));
+			result = map[N-1][N-1];
+			
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
+		}
+		System.out.println(sb.toString());
 	}
 	
-	
-	static int bfs(Point start) {
+	static void bfs(Point start) {
 		PriorityQueue<Point> pq = new PriorityQueue<>();
+		visited[start.x][start.y] = true;
 		pq.offer(start);
-		visited[start.x][start.y]=true;	
 		
 		while(!pq.isEmpty()) {
 			Point now = pq.poll();
 			
-			if(now.x == N-1 && now.y == N-1) {
-				return now.value;
-			}
+			if(now.x==N-1 && now.y==N-1) return;
 			
 			for(int i=0;i<4;i++) {
-				int nx = now.x + d[i][0];
-				int ny = now.y + d[i][1];
+				int nx = now.x+dx[i];
+				int ny = now.y+dy[i];
 				
-				if(nx >-1 && ny >-1 && nx<N && ny<N && !visited[nx][ny]) {
-					visited[nx][ny] = true;
-					pq.add(new Point(nx,ny,now.value + map[nx][ny]));
-				}
+				if(nx<0 || ny<0 || nx>=N || ny>=N || visited[nx][ny]) continue;
+
+				visited[nx][ny] = true;
+				map[nx][ny] += map[now.x][now.y];
+				pq.offer(new Point(nx,ny,map[nx][ny]));
 			}
 		}
-		
-		
-		return -1;
 	}
-	
+
 	static class Point implements Comparable<Point>{
-		int x;
-		int y;
-		int value;
+		int x,y,value;
 		public Point(int x, int y, int value) {
-			this.x = x;
-			this.y = y;
+			this.x=x;
+			this.y=y;
 			this.value = value;
 		}
+		
 		@Override
-		public int compareTo(Point o) {
-			return this.value - o.value;
+		public int compareTo(Point p) {
+			return this.value - p.value;
 		}
-	}	
+	}
 }
