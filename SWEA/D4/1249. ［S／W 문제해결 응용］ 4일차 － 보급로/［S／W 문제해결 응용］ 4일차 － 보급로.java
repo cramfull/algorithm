2125bf7632d5cf54ad3e6ee0bb07b61b6dd6bc1b
cramfull <130737411+cramfull@users.noreply.github.com>
2025-08-18 23:47:53 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 111 ms, Memory: 30,244 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -6,27 +6,29 @@ class Solution{
 	static int N, result;
 	static int [][] map;
 	static boolean [][] visited;
-	static int [] dx = {1,0,-1,0};
-	static int [] dy = {0,1,0,-1};
-
+	static int [] dx = {1,0,0,-1};
+	static int [] dy = {0,1,-1,0};
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
 		int TC = Integer.parseInt(br.readLine());
 		
-		for(int tc=1;tc<=TC;tc++) {
+		for(int tc=1; tc<=TC; tc++) {
 			N = Integer.parseInt(br.readLine());
+			
 			map = new int[N][N];
 			visited = new boolean[N][N];
 			
 			for(int i=0;i<N;i++) {
 				String line = br.readLine();
-				for(int j=0;j<N;j++) {
-					map[i][j] = line.charAt(j)-'0';
+				for(int j =0; j<N;j++) {
+					map[i][j] = line.charAt(j) - '0';
 				}
 			}
-			bfs(new Point(0,0,0));
-			result = map[N-1][N-1];
+			
+			result = 0;
+			bfs(new Point(0,0,map[0][0]));
 			
 			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
@@ -35,33 +37,36 @@ class Solution{
 	
 	static void bfs(Point start) {
 		PriorityQueue<Point> pq = new PriorityQueue<>();
+		
 		visited[start.x][start.y] = true;
 		pq.offer(start);
 		
 		while(!pq.isEmpty()) {
 			Point now = pq.poll();
-			
-			if(now.x==N-1 && now.y==N-1) return;
-			
+			if(now.x==N-1 && now.y==N-1) {
+				result = map[N-1][N-1];
+				return;
+			}
 			for(int i=0;i<4;i++) {
 				int nx = now.x+dx[i];
 				int ny = now.y+dy[i];
-				
 				if(nx<0 || ny<0 || nx>=N || ny>=N || visited[nx][ny]) continue;
-
+				
 				visited[nx][ny] = true;
 				map[nx][ny] += map[now.x][now.y];
-				pq.offer(new Point(nx,ny,map[nx][ny]));
+				pq.offer(new Point(nx, ny, map[nx][ny]));
 			}
+			
 		}
+		
 	}
 
 	static class Point implements Comparable<Point>{
 		int x,y,value;
-		public Point(int x, int y, int value) {
-			this.x=x;
-			this.y=y;
-			this.value = value;
+		public Point(int x,int y, int value) {
+			this.x= x;
+			this.y= y;
+			this.value= value;
 		}
 		
 		@Override
@@ -70,3 +75,4 @@ class Solution{
 		}
 	}
 }
+
