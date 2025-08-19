430b0b00e2c7c78c0da76ffe27ce6e92b5e42d07
cramfull <130737411+cramfull@users.noreply.github.com>
2025-08-19 11:58:30 +0900

    [D2] Title: 나무 높이, Time: 90 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,83 +1,63 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.*;
-import java.io.*;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
- static int N;
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	static int N, maxHeight;
 	static int [] trees, needWaters;
-	public static void main(String[] args) throws IOException {
+	
+	
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
 		int TC = Integer.parseInt(br.readLine());
 		
-		for(int tc=1; tc<=TC; tc++) {
+		for(int tc=1; tc<=TC;tc++) {
 			N = Integer.parseInt(br.readLine());
+			maxHeight = Integer.MIN_VALUE;
 			trees = new int[N];
-			int maxHeight = Integer.MIN_VALUE;
-			StringTokenizer str = new StringTokenizer(br.readLine());
+			needWaters = new int[N];
+			
+			StringTokenizer  str = new StringTokenizer(br.readLine());
 			for(int i=0;i<N;i++) {
-				trees[i]=Integer.parseInt(str.nextToken());
+				trees[i] = Integer.parseInt(str.nextToken());
 				maxHeight = Math.max(maxHeight, trees[i]);
 			}
-			needWaters = new int[N];
+			
 			for(int i=0;i<N;i++) {
 				needWaters[i] = maxHeight - trees[i];
 			}
 			
-			int result = getWateringTrees();
+			int resultDays = countDays();
 			
-			sb.append("#").append(tc).append(" ").append(result).append("\n");
+			sb.append("#").append(tc).append(" ").append(resultDays).append("\n");
 		}
+		
 		System.out.println(sb.toString());
 	}
-	static int getWateringTrees() {
-		int even = 0, odd = 0;
+	
+	static int countDays() {
+		int even = 0;
+		int odd = 0;
+		
 		for(int i=0;i<N;i++) {
 			even += needWaters[i]/2;
 			odd += needWaters[i]%2;
 		}
 		
-		if(odd==even) {
-			return even * 2;
-		}else if(odd > even) {
-			return odd * 2 - 1;
-		}else if(odd < even) {
+								// 홀-짝이 같을 때
+		if(odd == even) {
+			return even*2;
+		}else if(odd > even) { 	// 홀이 더 클 때
+			return odd*2-1;
+		}else { 				// 짝이 더 클 때
 			int n = (even - odd +1)/3;
-			even-=n;
-			odd+=2*n;
+			even -= n;
+			odd += 2*n;
 		}
 		
-		return (odd>even) ? odd*2-1 : even*2;
+		return (odd > even) ? odd*2-1 : even*2;
 	}
 }
