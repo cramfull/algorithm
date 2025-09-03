a81733cf3a1339458d74b20372641c5c22acbec1
cramfull <130737411+cramfull@users.noreply.github.com>
2025-09-03 11:06:06 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 125 ms, Memory: 32,000 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -6,9 +6,6 @@ public class Solution {
 	static int N,M,L,result;
 	static int [][] map;
 	static boolean [][] visited;
-	
-	static HashMap<Integer,Set<Delta>> deltaMap;
-	
 	static int [][][] delta = new int[][][] {
 		{},
 		{{-1,0},{1,0},{0,-1},{0,1}},
@@ -18,17 +15,7 @@ public class Solution {
 		{{1,0},{0,1}},
 		{{1,0},{0,-1}},
 		{{-1,0},{0,-1}}};
-		
-	static {
-		deltaMap = new HashMap<>();
-		for(int i=1;i<=7;i++) {
-			deltaMap.put(i,new HashSet<>());
-			for(int j=0;j<delta[i].length;j++) {
-				deltaMap.get(i).add(new Delta(delta[i][j][0],delta[i][j][1]));
-			}
-		}
-	}
-	
+
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -83,16 +70,7 @@ public class Solution {
 					int ny = now.y+deltaY;
 					
 					if(nx<0 || ny<0 || nx>=N || ny>=M || visited[nx][ny] || map[nx][ny]==0) continue;
-//					if(!(now.deltaX==0 && now.deltaY==0)) {
-//						if(!(deltaMap.get(map[nx][ny]).contains(new Delta(deltaX*-1,deltaY*-1)))) continue;
-////						int [][] checkDeltaArr = delta[map[nx][ny]];
-////						for(int j=0;j<checkDeltaArr.length;j++) {
-////							int checkDeltaX = checkDeltaArr[j][0];
-////							int checkDeltaY = checkDeltaArr[j][1];
-////							if(deltaX*-1==checkDeltaX && deltaY*-1==checkDeltaY) flag=true;
-////						}
-//		
-//					}
+
 					if(deltaX==0 && deltaY==-1){
 						if(map[nx][ny]==6 || map[nx][ny]==7 || map[nx][ny]==2) continue;
 					}else if(deltaX==0 && deltaY==1) {
@@ -103,7 +81,6 @@ public class Solution {
 						if(map[nx][ny]==4 || map[nx][ny]==7 || map[nx][ny]==3) continue;
 					}
 					
-					
 					visited[nx][ny] = true;
 					result++;
 					que.offer(new Point(nx,ny,deltaX,deltaY));
@@ -112,29 +89,6 @@ public class Solution {
 		}
 	}
 
-	static class Delta{
-		int x,y;
-		public Delta(int x, int y) {
-			this.x=x;
-			this.y=y;
-		}
-		@Override
-		public int hashCode() {
-			return Objects.hash(x, y);
-		}
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Delta other = (Delta) obj;
-			return x == other.x && y == other.y;
-		}
-	}
-	
 	static class Point{
 		int x,y,deltaX,deltaY;
 		public Point(int x, int y, int deltaX, int deltaY) {
