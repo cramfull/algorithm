afd76f0b9c5c1fa4dffc854dbe3427a179c371ae
cramfull <130737411+cramfull@users.noreply.github.com>
2025-09-09 12:04:46 +0900

    [D2] Title: 나무 높이, Time: 82 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,63 +1,61 @@
-
 import java.io.*;
 import java.util.*;
 
+/* 0909 11:30
+ * 나무높이
+ * 홀수 필요날, 짝수 필요날
+ * 
+ */
 public class Solution {
 	
-	static int N, maxHeight;
-	static int [] trees, needWaters;
+	static int N, maxHeight, result;
+	static int [] heights;
+	static int [] diff;
 	
-	
-	public static void main(String[] args) throws IOException{
+	public static void main(String [] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
 		int TC = Integer.parseInt(br.readLine());
 		
-		for(int tc=1; tc<=TC;tc++) {
+		for(int tc=1;tc<=TC;tc++) {
 			N = Integer.parseInt(br.readLine());
+			String [] line = br.readLine().split(" ");
+			heights = new int[N];
 			maxHeight = Integer.MIN_VALUE;
-			trees = new int[N];
-			needWaters = new int[N];
-			
-			StringTokenizer  str = new StringTokenizer(br.readLine());
 			for(int i=0;i<N;i++) {
-				trees[i] = Integer.parseInt(str.nextToken());
-				maxHeight = Math.max(maxHeight, trees[i]);
+				heights[i] = Integer.parseInt(line[i]);
+				maxHeight = Math.max(maxHeight, heights[i]);
 			}
 			
+			diff = new int[N];
 			for(int i=0;i<N;i++) {
-				needWaters[i] = maxHeight - trees[i];
+				diff[i]=maxHeight-heights[i];
 			}
 			
-			int resultDays = countDays();
+			int odd = 0;
+			int even = 0;
+			result = 0;
+			for(int i=0;i<N;i++) {
+				odd+=diff[i]%2;
+				even+=diff[i]/2;
+			}
+			if(odd==even) {
+				result = even*2;
+			}else if(odd>even) {
+				result = odd*2-1;
+			}else if(odd<even) {
+				int n = (even-odd+1)/3;
+				even-=n;
+				odd+=2*n;
+				if(odd<=even) {
+					result = even*2;
+				}else if(odd>even) {
+					result = odd*2-1;
+				}
+			}
 			
-			sb.append("#").append(tc).append(" ").append(resultDays).append("\n");
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
-		
 		System.out.println(sb.toString());
-	}
-	
-	static int countDays() {
-		int even = 0;
-		int odd = 0;
-		
-		for(int i=0;i<N;i++) {
-			even += needWaters[i]/2;
-			odd += needWaters[i]%2;
-		}
-		
-								// 홀-짝이 같을 때
-		if(odd == even) {
-			return even*2;
-		}else if(odd > even) { 	// 홀이 더 클 때
-			return odd*2-1;
-		}else { 				// 짝이 더 클 때
-			int n = (even - odd +1)/3;
-			even -= n;
-			odd += 2*n;
-		}
-		
-		return (odd > even) ? odd*2-1 : even*2;
 	}
 }
