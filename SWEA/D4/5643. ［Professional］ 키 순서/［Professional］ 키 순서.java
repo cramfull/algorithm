c92f04f8fce7a044ade6c1efd538e8b7e4dfcf74
cramfull <130737411+cramfull@users.noreply.github.com>
2025-09-10 16:00:14 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,509 ms, Memory: 108,864 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,53 +1,23 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-	
-	static int N, M;
+public class Solution {
+
+	static int N,M;
 	static ArrayList<ArrayList<Integer>> graph;
+	static boolean [] visited;
 	static ArrayList<ArrayList<Integer>> smallList, largeList;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+
+		int TC= Integer.parseInt(br.readLine());
 		
-		int TC = Integer.parseInt(br.readLine());
-		
-		for(int tc=1; tc<=TC; tc++) {
-			N = Integer.parseInt(br.readLine());
-			M = Integer.parseInt(br.readLine());
+		for(int tc=1;tc<=TC;tc++) {
+			N=Integer.parseInt(br.readLine());
+			M=Integer.parseInt(br.readLine());
+			
 			graph = new ArrayList<>();
 			smallList = new ArrayList<>();
 			largeList = new ArrayList<>();
@@ -62,7 +32,6 @@ class Solution
 				StringTokenizer str = new StringTokenizer(br.readLine());
 				int start = Integer.parseInt(str.nextToken());
 				int end = Integer.parseInt(str.nextToken());
-				
 				graph.get(start).add(end);
 			}
 			
@@ -70,33 +39,32 @@ class Solution
 				bfs(i);
 			}
 			
-			int count = 0;
+			int result = 0;
 			for(int i=1;i<=N;i++) {
-				if(smallList.get(i).size()+largeList.get(i).size() == N-1) count++;
+				if(smallList.get(i).size() + largeList.get(i).size() == N-1) result++;
 			}
-			
-			sb.append("#").append(tc).append(" ").append(count).append("\n");
+			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
-		
 		System.out.println(sb.toString());
 	}
 	
 	static void bfs(int start) {
-		Queue<Integer> que = new LinkedList<>();
-		boolean [] visited = new boolean[N+1];
-		visited[start] = true;
+		Queue<Integer> que = new ArrayDeque<>();
+		visited = new boolean[N+1];
 		que.offer(start);
+		visited[start] = true;
 		
 		while(!que.isEmpty()) {
 			int now = que.poll();
 			for(int i=0;i<graph.get(now).size();i++) {
-				int next = graph.get(now).get(i);
-				if(visited[next]) continue;
+				int nxt = graph.get(now).get(i);
 				
-				visited[next] = true;
-				que.offer(next);
-				largeList.get(start).add(next);
-				smallList.get(next).add(start);
+				if(visited[nxt]) continue;
+				
+				visited[nxt] = true;
+				smallList.get(nxt).add(start);
+				largeList.get(start).add(nxt);
+				que.offer(nxt);
 			}
 		}
 	}
